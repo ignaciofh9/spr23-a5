c61bd3c5089473b1701acb19a3fd99f2e02c7897
ignaciofh9 <80792211+ignaciofh9@users.noreply.github.com>
2023-05-12 17:16:24 -0700

    Update your_tests.py

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -8,6 +8,8 @@ from calculator_adapter import run
 assert run("99 + 0").output == "99"
 # Checks that the program outputs "-3" for an input of "-3 * 1"
 assert run("-3 * 1").output == "-3"
+# Checks that the program outputs "100.5" for an input of "201 / 2"
+assert run("201 / 2").output == "100.5"
 
 print("All tests passed!")
 ###
